d5adceeebca8f25ac6fe9fdab0d6aaa5e1514813
mblal <mohammed.blal@gmail.com>
2023-01-01 22:28:47 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export CICD_PROJECT_DIR=CICD_TNGR_APP
 export CICD_PROJECT_REPO=git@github.com:mblal/TNGR_APP_CIDC.git
 #@TODO The line below is not a good way, try to find how to get automatically in a specific submodule branch or tag
 export CICD_BRANCH=master
-
+export TARGET_ENV=rec
 git clone --recurse-submodules --branch $CICD_PROJECT_BRANCH_NAME $CICD_PROJECT_REPO $CICD_PROJECT_DIR
 cd $CICD_PROJECT_DIR
 #@TODO The Three lines below are to delete, try to find how to get automatically in a specific submodule branch or tag
